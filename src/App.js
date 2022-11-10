646171c30b122558b262a64e1c5e5651fe2ab064
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-09 18:09:54 -0700

    the App.js file in this commit will be mid throw away practice, commiting and adding a branch, locate my branch in the git repository to see it more organized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import React from "react"
 
+import React, { useState } from "react"
+
 export default function App() {
-	return <div>App</div>
+	let [objects, setObjects] = useState([
+		{ name: "guy", age: 7, tall: true },
+		{ name: "girl", age: 12, tall: false },
+	])
+	let newObj = {
+		name: "girl",
+		age: 12,
+		tall: false,
+	}
+
+	return (
+		<>
+			{objects[0].name}
+			<pre>{JSON.stringify(objects, null, 2)}</pre>
+			<button
+				onClick={(e) => {
+					setObjects([...objects, { name: "steve", age: 10, tall: true }])
+				}}
+			>
+				change name
+			</button>
+			<button
+				onClick={(e) => {
+					setObjects([...objects, newObj])
+				}}
+			>
+				add obj
+			</button>
+		</>
+	)
 }
+
+// function App() {
+// 	let [Todo, setTodo] = (0, React.useState)([""])
+// 	return React.default.createElement(SingleTodo, {
+// 		Todo: Todo,
+// 	})
+// }
