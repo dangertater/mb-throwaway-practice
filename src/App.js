25b18e21aff4986219e16ae4aaf8c97486b861cc
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-09 18:23:54 -0700

    object adders functioning, will commit this for a pull requst when eric gets back so i dont mess it up

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import React from "react"
-
 import React, { useState } from "react"
 
 export default function App() {
 	let [objects, setObjects] = useState([
-		{ name: "guy", age: 7, tall: true },
-		{ name: "girl", age: 12, tall: false },
+		{ name: "heather", age: 7, tall: true },
+		{ name: "bob", age: 12, tall: false },
 	])
-	let newObj = {
-		name: "girl",
+
+	//below are new variables that I make to implement into different functions/component practice
+	let newObj0 = {
+		name: "stan",
 		age: 12,
 		tall: false,
 	}
-
+	let newObj1 = {
+		name: "otto",
+		age: 15,
+		tall: false,
+	}
+	let newObj2 = {
+		name: "jud",
+		age: 48,
+		tall: true,
+	}
+	let newObjArray = [newObj0, newObj1, newObj2]
+	let oneTwoThree = () => {
+		return Math.floor(Math.random() * 3)
+	}
 	return (
 		<>
-			{objects[0].name}
+			{/* the below json.stringify with extra arguments is a convenient hack from eric. the pre's are essentially divs and the 3 arguments
+    make it appear in a very reader frinedly display on the localhost:3000*/}
 			<pre>{JSON.stringify(objects, null, 2)}</pre>
+			{/* the below button onClick will set call setObjects assosciated with the useState hook from above 
+      current syntax renders all previous objects, and then the new inline object containing 'steve'*/}
 			<button
 				onClick={(e) => {
 					setObjects([...objects, { name: "steve", age: 10, tall: true }])
 				}}
 			>
-				change name
+				add steve
 			</button>
+			{/*adds a random person from the newObjArray
+			 */}
 			<button
 				onClick={(e) => {
-					setObjects([...objects, newObj])
+					setObjects([...objects, newObjArray[oneTwoThree()]])
 				}}
 			>
-				add obj
+				add rando
 			</button>
 		</>
 	)
